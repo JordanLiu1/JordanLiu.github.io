1b2041497d523675d8e590ece0d977f89853bbd3
JordanLiu1 <1680jordan@gmail.com>
2024-08-06 19:50:29 +0000

    add a contact add list input contacts

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 function whenClick() {
-    alert("button clicked")
+    window.location.href = "idea.html";
 }
 let openpagebutton =  document.getElementById('learnmore');
 openpagebutton.addEventListener('click', whenClick)
 
-function whenClick() {
-    alert("button clicked")
-}
-let searchbarbutton =  document.getElementById('searchbutton');
-searchbarbutton.addEventListener('click', whenClick)
+// function whenClick() {
+//     alert("button clicked")
+// }
+// let searchbarbutton =  document.getElementById('searchbutton');
+// searchbarbutton.addEventListener('click', whenClick)
 
-let colorbutton = document.querySelector('#random-color')
+// let colorbutton = document.querySelector('#random-color')
 
-function changebackground() {
-    alert('clicked')
-    document.body.style.backgroundColor = "rgb(0, 0, 0)"
-}
-colorbutton.addEventListener('click', changebackground)
+// function changebackground() {
+//     alert('clicked')
+//     document.body.style.backgroundColor = "rgb(0, 0, 0)"
+// }
+// colorbutton.addEventListener('click', changebackground)
+
+document.getElementById('searchbutton').addEventListener('click', addcontact) 
+    function addcontact() {
+    let contact = document.getElementById('new-contact').value;
+    let li = document.createElement('li');
+
+    li.textContent = contact;
+
+    document.getElementById('contact-list').appendChild(li);
+
+    document.getElementById('new-contact').value = '';
+    alert("thankyou")
+};
+
